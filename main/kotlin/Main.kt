94b9be605b360d8257e6bb276c475022527c4a4a
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-09 10:43:57 +0300

    null values handling

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,13 +1,13 @@
 fun main(){
-    val x = 2+2
-    if (x ==2) {
-        println("x is 2")
-    }else if (x == 4){
-        println("x is 4")
-    }
-    else{
-        println("x != 2")
-    }
-    val y = if (x==2) 2 else 3
-    println(y)
+    val number1 = readLine()
+    val number2 = readLine()
+
+    val result = number1!!.toInt() + number2!!.toInt()
+    println(result)
+
+    val number3 = readLine() ?: "0"
+    val number4 = readLine() ?: "0"
+
+    val result2 = number3.toInt() + number4.toInt()
+    println(result2)
 }
