9262314ea1dc9369c70bcab6919ced7460a8d134
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-09 11:57:18 +0300

    using when statement

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -3,10 +3,12 @@ fun main(){
     val shopping = mutableListOf("toyota", "rolex", "mazda")
     shopping.add("Ferrari")
 
-     for (x in shopping){
-         println(x)
-    }
-    for (s in  1..50){
-        println(s)
+    val x = 3
+    when(x) {
+        in 1..2 -> println("x is between 1 and 2")
+        in 3..10 -> println("x is between 3 and 10")
+        else -> {
+            println("x is not in the range of 1 to 10")
+        }
     }
 }
