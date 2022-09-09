81a5921c33a1d1341223e21be4db690b58bea39f
Tumbo Lobi John <johnlobiit@gmail.com>
2022-09-09 11:47:43 +0300

    using for loop to loop through a list

--- main/kotlin/Main.kt
+++ main/kotlin/Main.kt
@@ -1,16 +1,12 @@
 fun main(){
-    // Immutable List
-    val shoppingList = listOf<String>("toyota", "rolex", "mazda")
-    println(shoppingList[1])
-
     // mutable List
     val shopping = mutableListOf("toyota", "rolex", "mazda")
     shopping.add("Ferrari")
-    println(shopping[3])
 
-    var counter = 0
-    while (counter < shopping.size) {
-        println(shopping[counter])
-        counter++
+     for (x in shopping){
+         println(x)
+    }
+    for (s in  1..50){
+        println(s)
     }
 }
